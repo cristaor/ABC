bbf493854abc2a74682df2c56d120c4cf5c0cab7
Franklin Candanoza <franklin.candanoza@bold.co>
2022-09-11 19:25:08 -0500

    Se adiciona inyección de errores para omitir efecto o afectar el payload

--- notification-processor/vistas/vistas.py
+++ notification-processor/vistas/vistas.py
@@ -1,6 +1,8 @@
+import os
 from http import client
 from multiprocessing import Event
 from flask import request
+from random import randint
 
 from modelos.modelos import Notification, NotificationSchema
 from modelos import db
@@ -33,6 +35,7 @@ class ReceivedNotificationRequestEvent:
     location_id: str
     sensor_type : SensorType
     event_type: EventType
+    instance: str
 
 celery_app = Celery(__name__, broker = 'redis://localhost:6379/0')
 
@@ -52,11 +55,13 @@ class VistaNotification(Resource):
         event = build_received_request_event(id = notification.extern_uuid, date_event =  notification.date_event, client_id =  notification.client_id, 
                                 location_id = notification.location_id, sensor_type = SensorType[notification.sensor_type], event_type=EventType[notification.event_type])
 
-        publish_received_request_event(event = event)
-
+        # Omitiendo la publicación del evento
+        publish_received_request_event_if_neccesary(event = event)
+        
         return {"msg": "Notificación recibida exitosamente"}
 
-
+def should_inject_error()->bool:
+    return randint(0, 20) > 17
 
 def build_received_request_event(id: str, date_event: datetime, client_id: str, 
                                 location_id: str, sensor_type : SensorType, 
@@ -65,14 +70,27 @@ def build_received_request_event(id: str, date_event: datetime, client_id: str,
     event = ReceivedNotificationRequestEvent()
     event.id = id
     event.date_event = date_event
-    event.client_id = client_id
+
+    if not should_inject_error():
+        event.client_id = client_id    
+    else:
+        print("Alterando el payload para la solicitud {} desde la instancia {}".format(id, os.environ.get('instance')))
+        event.client_id = 222
+
     event.location_id = location_id
     event.sensor_type = sensor_type
     event.event_type = event_type
+    event.instance = os.environ.get('instance')
 
     return event
 
-def publish_received_request_event(event : ReceivedNotificationRequestEvent)->None:
+def publish_received_request_event_if_neccesary(event : ReceivedNotificationRequestEvent)->None:
+    
+    if not should_inject_error():
+        args = ("event", json.dumps(event.__dict__, default=str))
+        publish_event.apply_async(args=args, queue= 'queue.notification.requested')
+    else:
+        print("Omitiendo evento con ID de solicitud {} desde la instancia {}".format(event.id, os.environ.get('instance')))
+       
 
-    args = ("event", json.dumps(event.__dict__, default=str))
-    publish_event.apply_async(args=args, queue= 'queue.notification.requested')
+    
